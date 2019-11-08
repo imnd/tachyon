da9f88789249ea54804b995e9851bc30bfca9c01
imnd <imndsk@gmail.com>
2019-11-08 15:02:25 +0700

    code enhancement

--- src/components/widgets/grid/views/_search.php
+++ src/components/widgets/grid/views/_search.php
@@ -5,20 +5,21 @@
     }
 </style>
 <?php
-use tachyon\dic\Container;
+use tachyon\dic\Container,
+    tachyon\components\html\FormBuilder;
 
 if (!empty($searchFields)) {
-    (new Container)->get('\tachyon\components\html\FormBuilder')
-        ->build(array(
-            'options' => array(
+    (new Container)->get(FormBuilder::class)
+        ->build([
+            'options' => [
                 'action' => "/{$widget->getController()->getId()}",
                 'class' => 'search-form',
                 'submitCaption' => $this->i18n('search'),
                 'view' => 'searchForm',
                 'viewsPath' => $widget->getViewPath(),
-            ),
+            ],
             'model' => $model,
             'fields' => $searchFields,
             'fieldValues' => $widget->getController()->getGet(),
-        ));
+        ]);
 }
